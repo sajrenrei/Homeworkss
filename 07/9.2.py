5743d2fc800ce74c96b846beb72bef62c4c6599d
sajrenrei <sajrenrei@gmail.com>
2026-07-03 11:44:01 +0300

    Homeworkss 9

---
+++ 07/9.2.py
@@ -0,0 +1,15 @@
+def difference(*args):
+    if not args:
+        return 0
+    max_val = max(args)
+    min_val = min(args)
+    diff = max_val - min_val
+    return round(diff, 2)
+
+
+assert difference(1, 2, 3) == 2, 'Test1'
+assert difference(5, -5) == 10, 'Test2'
+assert difference(10.2, -2.2, 0, 1.1, 0.5) == 12.4, 'Test3'
+assert difference() == 0, 'Test4'
+
+print('OK')
